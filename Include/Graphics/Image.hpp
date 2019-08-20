d1fb5308d4699db954086b89f34cf5a959f4a516
Vsevolod Bazhan <bazhan.ve@yandex.ru>
2019-08-20 20:45:47 +1000

    Introduce Image class
    An abstract base class that provides functionality for descended classes.

---
+++ Include/Graphics/Image.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "Pixel.hpp"
+#include "Properties.hpp"
+
+#include "Color/RGBColor.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <fstream>
+#include <list>
+#include <string>
+
+namespace Graphics {
+  class Image {
+  public:
+    using Pixels = std::list<Pixel>;
+    using PixelsIterator = Pixels::iterator;
+    using ConstPixelsIterator = Pixels::const_iterator;
+
+    Image(const Properties& properties);
+
+    virtual ~Image() = default;
+
+    virtual void Export(const std::string&) const = 0;
+
+    PixelsIterator begin();
+    PixelsIterator end();
+
+    const ConstPixelsIterator begin() const;
+    const ConstPixelsIterator end() const;
+
+    Properties GetProperties() const;
+
+  protected:
+    uint8_t ConvertToOctet(double compenent) const;
+
+  private:
+    Properties properties_;
+    std::list<Pixel> pixels_;
+  };
+}
---
+++ Source/Graphics/Image.cpp
@@ -0,0 +1,39 @@
+#include "Graphics/Image.hpp"
+
+namespace Graphics {
+  Image::Image(const Properties& properties) : properties_(properties), pixels_(properties.width * properties.height) {
+    Position current_position{0, 0};
+    for (Pixel& pixel : pixels_) {
+      pixel.position = current_position;
+      pixel.color = properties_.background;
+      if (++current_position.x == properties_.width) {
+        current_position.x = 0;
+        ++current_position.y;
+      }
+    }
+  }
+
+  Properties Image::GetProperties() const {
+    return properties_;
+  }
+
+  Image::PixelsIterator Image::begin() {
+    return pixels_.begin();
+  }
+
+  Image::PixelsIterator Image::end() {
+    return pixels_.end();
+  }
+
+  const Image::ConstPixelsIterator Image::begin() const {
+    return pixels_.begin();
+  }
+
+  const Image::ConstPixelsIterator Image::end() const {
+    return pixels_.end();
+  }
+
+  uint8_t Image::ConvertToOctet(double compenent) const {
+    return static_cast<uint8_t>(255 * compenent);
+  }
+}
