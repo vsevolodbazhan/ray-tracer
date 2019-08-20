b6cfb98e2f651380ff178bdea18f308b5a036976
Vsevolod Bazhan <bazhan.ve@yandex.ru>
2019-08-20 19:11:28 +1000

    Introduce Point2D and Point3D structs

---
+++ Include/Math/Point2D.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstddef>
+
+namespace Math {
+	template <typename Number>
+	struct Point2D {
+		Number x;
+		Number y;
+	};
+}
---
+++ Include/Math/Point3D.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstddef>
+
+namespace Math {
+	template <typename Number>
+	struct Point3D {
+		Number x;
+		Number y;
+		Number z;
+	};
+}
