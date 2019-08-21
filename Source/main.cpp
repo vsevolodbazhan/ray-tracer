e42120a3a8cd8f445f791d2c6430a498f5b69bb3
Vsevolod Bazhan <bazhan.ve@yandex.ru>
2019-08-22 00:35:33 +1000

    Update demo program

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,6 +6,8 @@
 #include "Object/Sphere.hpp"
 #include "Surface/Material.hpp"
 
+#include "profile.hpp"
+
 using namespace Color;
 using namespace Core;
 using namespace Graphics;
@@ -18,11 +20,12 @@ int main() {
   Material ivory{RGBColor(0.4, 0.4, 0.3)};
   Material red_rubber{RGBColor(0.3, 0.1, 0.1)};
 
-  Scene scene(make_unique<PPMImage>(ImageProperties{1024, 768}));
+  Scene scene(make_unique<PPMImage>(ImageProperties{1920, 1080}), Camera{{0, 0, 0}, 45});
   scene.AddShape(make_unique<Sphere>(ivory, Vector3D{-3, 0, -16}, 2));
   scene.AddShape(make_unique<Sphere>(red_rubber, Vector3D{-1, -1.5, -12}, 2));
   scene.AddShape(make_unique<Sphere>(red_rubber, Vector3D{1.5, -0.5, -18}, 3));
   scene.AddShape(make_unique<Sphere>(ivory, Vector3D{7, 5, -18}, 4));
 
-  scene.Render("Example/Ambient");
+  scene.Render("Example/Ambient (No AA)");
+  scene.Render("Example/Ambient (AA)", true);
 }
