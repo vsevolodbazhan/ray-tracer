9760f3374bf5476016d84c9a080a3e6f1aae1cbe
Vsevolod Bazhan <bazhan.ve@yandex.ru>
2019-08-22 10:47:05 +1000

    Add RayHit struct

--- Include/Core/Ray.hpp
+++ Include/Core/Ray.hpp
@@ -4,11 +4,18 @@
 #include "Graphics/Pixel.hpp"
 #include "Math/Vector3D.hpp"
 #include "Object/Camera.hpp"
+#include "Surface/Material.hpp"
 
 namespace Core {
   struct Ray {
     Math::Vector3D origin;
     Math::Vector3D direction;
+  };
+
+  struct RayHit {
+    Math::Vector3D intersection;
+    Math::Vector3D normal;
+    Surface::Material material;
   };
 
   Ray CreateRay(const Graphics::PixelPosition& position, const Graphics::PixelOffset& offset,
